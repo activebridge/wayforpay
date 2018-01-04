3e5f6561193beba9212e8c7bc1b60c58042292f2
YaroslavTupitskyi <jaroslav.typitsky@gmail.com>
2018-01-04 16:38:34 +0200

    Initialize params for 'Request.call' method

--- lib/wayforpay/request.rb
+++ lib/wayforpay/request.rb
@@ -3,9 +3,9 @@ require 'net/http'
 
 module Wayforpay
   class Request
-    def self.call(encrypted_fields, request_attrs)
-      request_attrs[:merchantSignature] = EncryptField.(encrypted_fields, request_attrs)
-      Net::HTTP.post(Constants::URL, request_attrs.to_json)
+    def self.call(encrypted_fields = [], request_params = {})
+      request_params[:merchantSignature] = EncryptField.(encrypted_fields, request_params)
+      Net::HTTP.post(Constants::URL, request_params.to_json)
     end
   end
 end
